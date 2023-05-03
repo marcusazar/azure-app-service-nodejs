1e0486c52680a213ad98f341c3669492309477d7
Marcus Azara <marcus.azara@forefront.se>
2023-05-03 08:53:13 +0200

    testing staging

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { airplaneName: 'Airbus A340' });
+  res.render('index', { airplaneName: 'Airbus A340 testing staging!' });
 });
 
 module.exports = router;
